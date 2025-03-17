733d2041c5de77c83a86e3b755240d8a570bb76a
tuan1233734 <128621910+tuan1233734@users.noreply.github.com>
2025-03-17 19:33:36 +0700

    du an chinh

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,31 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class UserController {
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "hello";
+    }
+
+}
+
+// @RestController
+
+// public class UserController {
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
